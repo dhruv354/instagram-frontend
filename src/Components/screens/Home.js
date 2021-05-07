336aea232c025377893a427dfedc9cf538730cf7
dhruv singhal <dhruv.singhal2612@gmail.com>
2021-05-07 21:50:03 +0530

    added like and unlike feature on home page

--- src/Components/screens/Home.js
+++ src/Components/screens/Home.js
@@ -8,16 +8,69 @@ function Home() {
   //logic for avoiding home display without being signed in
   const history = useHistory();
   const { state, dispatch } = useContext(UserContext);
-  if (!state) {
-    M.toast({
-      html: "please signin",
-      classes: "#e57373 red lighten-2",
-    });
-    history.push("/signin");
-  }
+  // if (!state) {
+  //   M.toast({
+  //     html: "please signin",
+  //     classes: "#e57373 red lighten-2",
+  //   });
+  //   history.push("/signin");
+  // } else {
+  //   history.push("/");
+  // }
 
   //logic to  fetch all posts from database
   const [data, setData] = useState([]);
+
+  const likePost = (id) => {
+    fetch("/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData = data.map((post) => {
+          if (post._id == result._id) {
+            return result;
+          } else {
+            return post;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const unLikePost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData = data.map((post) => {
+          if (post._id == result._id) {
+            return result;
+          } else {
+            return post;
+          }
+        });
+        setData(newData);
+      });
+  };
 
   useEffect(() => {
     fetch("http://localhost:8000/allposts", {
@@ -47,6 +100,16 @@ function Home() {
               <i className="material-icons" style={{ color: "red" }}>
                 favorite
               </i>
+              <i className="material-icons" onClick={() => likePost(post._id)}>
+                thumb_up
+              </i>
+              <i
+                className="material-icons"
+                onClick={() => unLikePost(post._id)}
+              >
+                thumb_down
+              </i>
+              <h6>{post.likes.length} likes</h6>
               <h6>{post.title}</h6>
               <p>{post.body}</p>
               <input type="text" placeholder="add a comment" />
